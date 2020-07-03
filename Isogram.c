6fcfb9596fa64e18919a566d11a2f4e082657bbd
sushmaannigeri <64304098+sushmaannigeri@users.noreply.github.com>
2020-07-03 10:50:47 +0530

    Create Isogram.c

---
+++ Isogram.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    char str1[50];
+    int repeat = 0;
+    printf("Enter word\n");
+    scanf("%s",str1);
+
+    for (int i = 0; i < strlen(str1); i++) {
+        for (int j = i + 1; j < strlen(str1); j++) {
+            if (str1[i] == str1[j]) {
+                repeat = 1;
+                break;
+            }
+            if (repeat == 1) {
+                break;
+            }
+        }
+    }
+
+    if (repeat == 1) {
+        printf("The word is not an isogram\n");
+    }
+    else {
+        printf("The word is an isogram\n");
+    }
+}
